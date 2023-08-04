2d86d7dd2557ca7845a00f943101ac29ee58590d
YKK <yugeshkk2001@gmail.com>
2023-08-04 14:46:29 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 
 let masterplay=document.getElementById('masterplay');
-let audio= document.getElementById('firstSong');
+let next= document.getElementById('next');
+let songindex=0;
+let audio=new Audio;
 let play='pause';
 
+let songs=[
+    "song/Pottakaatil-Poovasam-MassTamilan.com.mp3",
+    "song/Vaa-Rayil-Vida-Polaama-MassTamilan.com.mp3"
+]
 
+let index=0;
+let len= songs.length;
 
 masterplay.addEventListener('click',()=>{
     if(play=='pause'){
-        audio.play();
-        masterplay.src="images/icons8-pause-50.png";
-        play='play'
+        if(index>=0){
+            audio.src=songs[index];
+            audio.play();
+            masterplay.src="images/icons8-pause-50.png";
+            play='play'
+            console.log(len)
+        }
     }else{
         audio.pause();
         masterplay.src="images/icons8-play-30.png";
         play='pause';
     }
 })
+
+next.addEventListener('click',()=>{
+    if(index>=len-1){
+        index=0;
+        audio.src=songs[index];
+        audio.play();
+        play='pause';
+    }else{
+        index++;
+        audio.src=songs[index];
+        audio.play();
+        play='pause';
+    }
+} )
 
